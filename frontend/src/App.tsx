fdd7a727b37073b05d36609413f7d3babaae2928
João Paulo de Melo Silva <joaopaulomelosilva@gmail.com>
2022-11-19 16:52:48 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,13 @@ import Header from "./Components/Header"
 import SalesCard from "./Components/SalesCard"
 import NotificationButton from "./Components/NotificationButton"
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
 
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
