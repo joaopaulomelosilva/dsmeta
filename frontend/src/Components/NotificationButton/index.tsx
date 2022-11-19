dad23d2d52366504d3564ad73a75055e22213b65
João Paulo de Melo Silva <joaopaulomelosilva@gmail.com>
2022-11-19 18:15:09 -0300

    Fix CSS2

--- frontend/src/Components/NotificationButton/index.tsx
+++ frontend/src/Components/NotificationButton/index.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import icon from "../../assets/img/notification-icon.svg";
 import { BASE_URL } from "../../utils/request";
-import "./styles.css"
+import "./styles.css";
 
 type Props = {
     saleId: number;
@@ -11,7 +11,7 @@ type Props = {
 function handleClick(id: number){
     axios(`${BASE_URL}/sales/${id}/notification`)
     .then(response => {
-        toast.info("SMS enviado com sucesso! ")
+        toast.info("SMS enviado com sucesso! ");
     });
 }
 
